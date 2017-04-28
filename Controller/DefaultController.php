2240561677a52faa7b4f67eb4d895108403a4421
igorl@gmail.com <igore4ek061@rambler.ru>
2017-04-28 18:03:47 +0300

    add return value for DefaultController

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,11 +4,11 @@ class DefaultController
 {
     public function indexAction()
     {
-        
+        return 'wlcome text';
     }
     
     public function aboutAction()
     {
-        
+        return 'contact form';
     }
 }
