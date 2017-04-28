f2a213a3d8051fb941144c326130a0ee3d0fb03a
igorl@gmail.com <igore4ek061@rambler.ru>
2017-04-28 18:48:21 +0300

    add static call for render() method in base Controller

--- Controller/BookController.php
+++ Controller/BookController.php
@@ -4,18 +4,6 @@ class BookController extends Controller
 {
     public function indexAction()
     {
-    	/*$dir = str_replace('Controller', '', __CLASS__);
-    	$file = VIEW_DIR . $dir . DS . 'index.phtml';
-
-    	if( !file_exists($file) )
-    	{
-    		throw new Exception("{$file} is not exist");
-    	}
-
-        ob_start();
-        require $file;
-        return ob_get_clean();
-        */
         return static::render('index.phtml');
     }
     
--- Library/Controller.php
+++ Library/Controller.php
@@ -2,22 +2,6 @@
 
 abstract class Controller
 {
-	/*
-	public function render($view)
-	{
-		$dir = str_replace('Controller', '', get_class($this));
-    	$file = VIEW_DIR . $dir . DS . $view;
-
-    	if( !file_exists($file) )
-    	{
-    		throw new Exception("{$file} is not exist");
-    	}
-
-        ob_start();
-        require $file;
-        return ob_get_clean();
-	}
-	*/
 
 	public static function render($view)
 	{
