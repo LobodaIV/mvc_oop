80fefea44d3a5775bc1149e66e414710632fd394
igorl@gmail.com <igore4ek061@rambler.ru>
2017-05-03 17:17:51 +0300

    change code for BookController.php add Model for it

--- Controller/BookController.php
+++ Controller/BookController.php
@@ -1,13 +1,25 @@
 <?php
 
 namespace Controller;
+
 use Library\Controller;
+use Model\BookModel;
 
 class BookController extends Controller
 {
     public function indexAction()
     {
-        return static::render('index.phtml');
+        $model = new BookModel();
+        $books = $model->findAll();
+        
+        $author = 'King';
+        
+        $data = [
+            'author' => $author, 
+            'books' => $books
+        ];
+        
+        return $this->render('index.phtml', $data);    
     }
     
     public function showAction()
