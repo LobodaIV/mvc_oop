976c334107ccd6165b38ea3f01052137d5415a1b
igorl@gmail.com <igore4ek061@rambler.ru>
2017-05-03 17:23:06 +0300

    add DBConnection model

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,17 +1,34 @@
 <?php
 
 namespace Controller;
+
 use Library\Controller;
+use Model\Form\FeedbackForm;
+use Model\FeedbackModel;
+use Library\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return 'wlcome text';
+        return $this->render('index.phtml');
     }
     
-    public function aboutAction()
+    public function feedbackAction(Request $request)
     {
-        return 'contact form';
+        $form = new FeedbackForm($request);
+        
+        if ($request->isPost()) {
+            if ($form->isValid()) {
+                $model = new FeedbackModel();
+                $model->save([
+                    'author' => $form->author,
+                    'email' => $form->email,
+                    'message' => $form->message,
+                ]);
+            }
+        }
+        
+        return $this->render('feedback.phtml');
     }
 }
