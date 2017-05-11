71b09a9adfd151031c8b646910d69aad8d637fd9
igorl@gmail.com <igore4ek061@rambler.ru>
2017-05-11 21:50:38 +0300

    move to webroot directory

---
+++ webroot/index.php
@@ -0,0 +1,51 @@
+<?php
+error_reporting(E_ALL);
+
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', __DIR__ . DS . '..' . DS); // ../
+define('VIEW_DIR', ROOT.'View' . DS);
+
+spl_autoload_register( function($className) {
+    
+    $file = ROOT . str_replace('\\', DS, "{$className}.php");
+    //echo $file;
+    if( !file_exists($file) )
+    {
+    	throw new \Exception("File not found");
+    }
+
+    require_once $file;
+	    
+});
+
+\Library\Session::start();
+
+$request = new \Library\Request();
+$container = new \Library\Container();
+$container->set('router', new \Library\Router());
+
+$route = $request->get('route', 'default/index'); // $_GET['route']
+
+//todo: защита от дурака если нет слэш в значении
+
+if( !preg_match("//", $route) ) {
+	throw new \Exception("Wrong format for controller");
+}
+
+$route = explode('/', $route);
+
+$controller = 'Controller' . DS . ucfirst($route[0]) . 'Controller';
+$action = $route[1] . 'Action';
+
+//$controller = new $controller();//Controller\DefaultController
+
+$controller = (new $controller())->setContainer($container);
+
+if (!method_exists($controller, $action)) {
+    throw new \Exception("{$action} not found");
+}
+
+echo $controller->$action($request);
+
+require VIEW_DIR . 'layout.phtml';
